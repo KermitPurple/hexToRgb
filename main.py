0e2231e9d58d8f9942cb55b075c2a5ae807e793d
Shane McDonough <shanemcdman@gmail.com>
2020-07-17 11:56:17 -0700

    Removing leading #

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def hex_to_rgb(hex_str: str) -> tuple:
+    if hex_str.startswith("#"):
+        hex_str = hex_str[1:]
+    return hex_str
+
 def main():
-    pass
+    print(hex_to_rgb("#000000"))
 
 if __name__ == "__main__":
     main()
